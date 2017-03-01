dfaa7c738e29a542e12fca06e40af94ddba6760f
tolgahanuzun <mail@tolgahanuzun.com>
2017-03-01 17:27:12 +0300

    New func add

--- bulanik-grafik.py
+++ bulanik-grafik.py
@@ -15,3 +15,48 @@ def ayrikgrafik():
 
 	plt.savefig('example/bulanik-ayrikyapi.png')
 	plt.show() 
+
+
+def surekligrafik():
+	indisy =  np.arange(0.0, 10.1, 0.1)
+	indisx = []
+	for indis in indisy:
+		indisx.append(1./(1+10*((indis-5)**2)))
+	indisx =np.array(indisx)	
+	
+	fig = plt.figure(figsize=(6, 6))
+	vax = fig.add_subplot(111)
+	vax.plot(indisy,indisx)
+
+	plt.savefig('example/bulanik-surekliyapi.png')
+	plt.show() 
+
+	
+
+def ucgenuyelik():
+	indisy = np.arange(0.0, 5.1, 0.01)
+	indisx = []
+	x1=1
+	xT=3
+	x2=4
+	for x in indisy:
+		a=(x-x1)/(xT-x1)
+		b=(x2-x)/(x2-xT)
+		c=0;
+		indisx.append(max(min(a,b),c))
+
+	indisx = np.array(indisx)
+	
+	fig = plt.figure(figsize=(6, 6))
+	vax = fig.add_subplot(111)
+	vax.plot(indisy,indisx)
+
+	l, = vax.plot(np.repeat(1, 6), '--')
+	plt.axvline(3, color='b', linestyle='dashed', linewidth=2)
+
+	plt.savefig('example/ucgenuyelik.png')
+	plt.show()
+
+ucgenuyelik()	
+surekligrafik()
+ayrikgrafik()
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 # Bulanik Mantik Uygulamalari
 
 ## Bulanik Mantik icin Basit grafik cizimi denemeleri.
+
+![Ayrik Yapi](example/bulanik-ayrikyapi.png)
+<hr>
+![Surekli Yapi](example/bulanik-surekliyapi.png)
+<hr>
+![Ucgen uyelik](example/ucgenuyelik.png)
